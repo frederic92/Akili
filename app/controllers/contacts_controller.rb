77d308c39d7dce613512a1f90a33599681bd5c68
Frederic Mbea <fredericmbea@gmail.com>
2016-12-29 04:25:05 +0000

    Modified flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,10 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
+            flash[:success] = "Message sent."
+            redirect_to new_contact_path
         else
+            flash[:danger] = @contact.errors.full_messages.join(", ")
             redirect_to new_contact_path, notice: "Error(s) occured."
         end
     end
