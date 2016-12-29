c31f10c2f4227b4aa541b694b161237182b7d49b
Frederic Mbea <fredericmbea@gmail.com>
2016-12-29 06:17:06 +0000

    Added contact mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+    default to: "fredericmbea@yahoo.fr"
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: "Contact form message")
+    end
+end
