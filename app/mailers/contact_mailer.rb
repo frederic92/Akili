eb4f2186690dd452ba1047e761b3e434880226f7
Frederic Mbea <fredericmbea@gmail.com>
2016-12-30 02:12:01 +0000

    Update email address in the Contact_mailer file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: "fredericmbea@yahoo.fr"
+    default to: "fredericmbea@gmail.com"
     
     def contact_email(name, email, body)
         @name = name
